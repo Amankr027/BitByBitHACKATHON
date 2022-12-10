94ee9789908e8fa1895aa01bcbeabe88ac3964cd
amankr027 <amanvermay27@gmail.com>
2022-12-10 19:15:00 +0530

    major changes

--- assets/js/mongdb.js
+++ assets/js/mongdb.js
@@ -1,16 +1,28 @@
-const {MongoClient}=require("mongodb"); 
-const url="mongodb://127.0.0.1:27017?directConnection-true"; 
-const client=new MongoClient(url);
+var x;
+ function getData(){
+    url = "http://127.0.0.1:5501/diseases.json"; 
+    fetch(url).then((response)=>{
+    
+     return response.json();
+    
+    }).then((data)=>{
+     
+    // console.log(data[0].name);
+    x=data;
+    })
+    
+    }
+     getData()
 
-const database="Dis";
- async function getData(){
-
-let results= await client.connect();
- let db =result.db(database);
-
-let collection =db.collection("bcde");
- let response=await collection.find({symp:"fever"}).toArray(); }
-
-console.log(response);
-
-getData();
+     function clicked(){
+        let selected= document.getElementById("abc").value;
+        // console.log(selected);
+            
+           for(let i=0; i<x.length; i++)
+                if(selected==x[i].name)
+                {
+                    console.log(x[i].desc);
+                }
+            
+        
+     }
